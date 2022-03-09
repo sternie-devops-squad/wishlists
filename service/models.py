1d1b9e560d029c3c9d5c27f85129fe52e29716a9
Aaron Weber <avw8176@stern.nyu.edu>
2022-03-09 14:36:04 -0500

    Following John's advice and stripping out existing service and test logic for clean slate

--- service/models.py
+++ service/models.py
@@ -1,36 +1,9 @@
-# Copyright 2016, 2021 John Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
-Models for Pet Demo Service
+Models for YourResourceModel
 
 All of the models are stored in this module
-
-Models
-------
-Pet - A Pet used in the Pet Store
-
-Attributes:
------------
-name (string) - the name of the pet
-category (string) - the category the pet belongs to (i.e., dog, cat)
-available (boolean) - True for pets that are available for adoption
-
 """
 import logging
-from enum import Enum
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -39,215 +12,105 @@ logger = logging.getLogger("flask.app")
 db = SQLAlchemy()
 
 
-def init_db(app):
-    """Initialize the SQLAlchemy app"""
-    Pet.init_db(app)
-
-
 class DataValidationError(Exception):
-    """Used for an data validation errors when deserializing"""
+    """ Used for an data validation errors when deserializing """
+
+    pass
 
 
-class Gender(Enum):
-    """Enumeration of valid Pet Genders"""
-
-    MALE = 0
-    FEMALE = 1
-    UNKNOWN = 3
-
-
-class Pet(db.Model):
+class YourResourceModel(db.Model):
     """
-    Class that represents a Pet
-
-    This version uses a relational database for persistence which is hidden
-    from us by SQLAlchemy's object relational mappings (ORM)
+    Class that represents a <your resource model name>
     """
 
-    ##################################################
+    app = None
+
     # Table Schema
-    ##################################################
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63), nullable=False)
-    category = db.Column(db.String(63), nullable=False)
-    available = db.Column(db.Boolean(), nullable=False, default=False)
-    gender = db.Column(
-        db.Enum(Gender), nullable=False, server_default=(Gender.UNKNOWN.name)
-    )
-
-    ##################################################
-    # INSTANCE METHODS
-    ##################################################
+    name = db.Column(db.String(63))
 
     def __repr__(self):
-        return "<Pet %r id=[%s]>" % (self.name, self.id)
+        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
 
     def create(self):
         """
-        Creates a Pet to the database
+        Creates a YourResourceModel to the database
         """
         logger.info("Creating %s", self.name)
-        # id must be none to generate next primary key
-        self.id = None  # pylint: disable=invalid-name
+        self.id = None  # id must be none to generate next primary key
         db.session.add(self)
         db.session.commit()
 
-    def update(self):
+    def save(self):
         """
-        Updates a Pet to the database
+        Updates a YourResourceModel to the database
         """
         logger.info("Saving %s", self.name)
-        if not self.id:
-            raise DataValidationError("Update called with empty ID field")
         db.session.commit()
 
     def delete(self):
-        """Removes a Pet from the data store"""
+        """ Removes a YourResourceModel from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
-    def serialize(self) -> dict:
-        """Serializes a Pet into a dictionary"""
-        return {
-            "id": self.id,
-            "name": self.name,
-            "category": self.category,
-            "available": self.available,
-            "gender": self.gender.name,  # convert enum to string
-        }
+    def serialize(self):
+        """ Serializes a YourResourceModel into a dictionary """
+        return {"id": self.id, "name": self.name}
 
-    def deserialize(self, data: dict):
+    def deserialize(self, data):
         """
-        Deserializes a Pet from a dictionary
+        Deserializes a YourResourceModel from a dictionary
+
         Args:
-            data (dict): A dictionary containing the Pet data
+            data (dict): A dictionary containing the resource data
         """
         try:
             self.name = data["name"]
-            self.category = data["category"]
-            if isinstance(data["available"], bool):
-                self.available = data["available"]
-            else:
-                raise DataValidationError(
-                    "Invalid type for boolean [available]: "
-                    + str(type(data["available"]))
-                )
-            self.gender = getattr(Gender, data["gender"])  # create enum from string
-        except AttributeError as error:
-            raise DataValidationError("Invalid attribute: " + error.args[0])
         except KeyError as error:
-            raise DataValidationError("Invalid pet: missing " + error.args[0])
+            raise DataValidationError(
+                "Invalid YourResourceModel: missing " + error.args[0]
+            )
         except TypeError as error:
             raise DataValidationError(
-                "Invalid pet: body of request contained bad or no data " + str(error)
+                "Invalid YourResourceModel: body of request contained bad or no data"
             )
         return self
 
-    ##################################################
-    # CLASS METHODS
-    ##################################################
-
     @classmethod
-    def init_db(cls, app: Flask):
-        """Initializes the database session
-
-        :param app: the Flask app
-        :type data: Flask
-
-        """
+    def init_db(cls, app):
+        """ Initializes the database session """
         logger.info("Initializing database")
+        cls.app = app
         # This is where we initialize SQLAlchemy from the Flask app
         db.init_app(app)
         app.app_context().push()
         db.create_all()  # make our sqlalchemy tables
 
     @classmethod
-    def all(cls) -> list:
-        """Returns all of the Pets in the database"""
-        logger.info("Processing all Pets")
+    def all(cls):
+        """ Returns all of the YourResourceModels in the database """
+        logger.info("Processing all YourResourceModels")
         return cls.query.all()
 
     @classmethod
-    def find(cls, pet_id: int):
-        """Finds a Pet by it's ID
-
-        :param pet_id: the id of the Pet to find
-        :type pet_id: int
-
-        :return: an instance with the pet_id, or None if not found
-        :rtype: Pet
-
-        """
-        logger.info("Processing lookup for id %s ...", pet_id)
-        return cls.query.get(pet_id)
+    def find(cls, by_id):
+        """ Finds a YourResourceModel by it's ID """
+        logger.info("Processing lookup for id %s ...", by_id)
+        return cls.query.get(by_id)
 
     @classmethod
-    def find_or_404(cls, pet_id: int):
-        """Find a Pet by it's id
-
-        :param pet_id: the id of the Pet to find
-        :type pet_id: int
-
-        :return: an instance with the pet_id, or 404_NOT_FOUND if not found
-        :rtype: Pet
-
-        """
-        logger.info("Processing lookup or 404 for id %s ...", pet_id)
-        return cls.query.get_or_404(pet_id)
+    def find_or_404(cls, by_id):
+        """ Find a YourResourceModel by it's id """
+        logger.info("Processing lookup or 404 for id %s ...", by_id)
+        return cls.query.get_or_404(by_id)
 
     @classmethod
-    def find_by_name(cls, name: str) -> list:
-        """Returns all Pets with the given name
+    def find_by_name(cls, name):
+        """Returns all YourResourceModels with the given name
 
-        :param name: the name of the Pets you want to match
-        :type name: str
-
-        :return: a collection of Pets with that name
-        :rtype: list
-
+        Args:
+            name (string): the name of the YourResourceModels you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
-
-    @classmethod
-    def find_by_category(cls, category: str) -> list:
-        """Returns all of the Pets in a category
-
-        :param category: the category of the Pets you want to match
-        :type category: str
-
-        :return: a collection of Pets in that category
-        :rtype: list
-
-        """
-        logger.info("Processing category query for %s ...", category)
-        return cls.query.filter(cls.category == category)
-
-    @classmethod
-    def find_by_availability(cls, available: bool = True) -> list:
-        """Returns all Pets by their availability
-
-        :param available: True for pets that are available
-        :type available: str
-
-        :return: a collection of Pets that are available
-        :rtype: list
-
-        """
-        logger.info("Processing available query for %s ...", available)
-        return cls.query.filter(cls.available == available)
-
-    @classmethod
-    def find_by_gender(cls, gender: Gender = Gender.UNKNOWN) -> list:
-        """Returns all Pets by their Gender
-
-        :param gender: values are ['MALE', 'FEMALE', 'UNKNOWN']
-        :type available: enum
-
-        :return: a collection of Pets that are available
-        :rtype: list
-
-        """
-        logger.info("Processing gender query for %s ...", gender.name)
-        return cls.query.filter(cls.gender == gender)
