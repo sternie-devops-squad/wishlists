7360b1f0d240558b5e00b1931cc70946c5d2036d
Sean Bates <sb8414@stern.nyu.edu>
2022-03-22 01:55:08 +0000

    Added update wishlist item and get item tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -179,7 +179,7 @@ class TestWishlistService(TestCase):
         )
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-    # Error handler testing code below based on the Service_Accounts code example
+    # Error handler testing code below based on the Service_Wishlists code example
     def test_bad_request(self):
         """ Send wrong media type """
         wishlist = WishlistFactory()
@@ -263,3 +263,73 @@ class TestWishlistService(TestCase):
         self.assertEqual(data["price"], item.price)
         self.assertEqual(data["in_stock"], item.in_stock)
         self.assertEqual(data["purchased"], item.purchased)
+
+    def test_get_item(self):
+        """ Get an item from a wishlist """
+        # create a known item
+        wishlist = self._create_wishlists(1)[0]
+        item = ItemFactory()
+        resp = self.app.post(
+            f"{BASE_URL}/{wishlist.id}/items",
+            json=item.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+
+        # retrieve it back
+        resp = self.app.get(
+            f"{BASE_URL}/{wishlist.id}/items/{item_id}",
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["wishlist_id"], wishlist.id)
+        self.assertEqual(data["name"], item.name)
+        self.assertEqual(data["category"], item.category)
+        self.assertEqual(data["price"], item.price)
+        self.assertEqual(data["in_stock"], item.in_stock)
+        self.assertEqual(data["purchased"], item.purchased)
+
+    def test_update_item(self):
+        """ Update an item in a wishlist """
+        # create a known item
+        wishlist = self._create_wishlists(1)[0]
+        item = ItemFactory()
+        resp = self.app.post(
+            f"{BASE_URL}/{wishlist.id}/items", 
+            json=item.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+        data["name"] = "XXXX"
+
+        # send the update back
+        resp = self.app.put(
+            f"{BASE_URL}/{wishlist.id}/items/{item_id}",
+            json=data, 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        # retrieve it back
+        resp = self.app.get(
+            f"{BASE_URL}/{wishlist.id}/items/{item_id}",
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["id"], item_id)
+        self.assertEqual(data["wishlist_id"], wishlist.id)
+        self.assertEqual(data["name"], "XXXX")
